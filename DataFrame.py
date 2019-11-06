217b0c0c37c57b0c935d4dd3f21ab568a1cbe1f6
Spasov <51121651+enerhy@users.noreply.github.com>
2019-11-06 10:50:47 +0200

    Update DataFrame.py

--- DataFrame.py
+++ DataFrame.py
@@ -3,13 +3,19 @@
 df.head()
 df.describe()
 
+#Describing of a single Feature over the binary reslut
+df.groupby('Survived').Fare.describe()
+
 #Null Values / Nan
 df.isnull().any()
 df.isnull().sum()
 df.isnull().sum() / df.shape[0]         #in Percentage
 
-#Counting unique values of a feature
+#Counting apearance of values of a feature
 df.Embarked.value_counts() #/NaN values not included
+#Counting the apearance of a specific value of a feature
+df[df.Sex == 'female'].Survived.count()
+
 
 #Stats of values 
 df['Age'].min()
@@ -26,6 +32,12 @@ ag = df.groupby(['Pclass', 'Survived'])['PassengerId'].count()
 
 #to make it a DataFrame
 ag1 = ag.reset_index()
+
+#Operations by a group
+df.groupby(['Pclass']).Survived.sum()
+ag4 = df.groupby('Survived')['Age'].mean()
+ag5 = df.groupby('Survived')['Age'].std()
+
 
 
 
