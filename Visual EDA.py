17614397ccdd28378d747afa7ed3fb6229c47d34
Spasov <51121651+enerhy@users.noreply.github.com>
2019-11-06 10:50:41 +0200

    Update Visual EDA.py

--- Visual EDA.py
+++ Visual EDA.py
@@ -10,3 +10,15 @@ sns.set()
 sns.countplot(x='Survived', data=df)
 #Counting separated with aditional feature (similar to Pivot)
 sns.factorplot(x='Survived', col='Sex', kind='count', data=df);
+
+
+#Histogramm
+sns.distplot(df.Fare, kde=False);
+#Histogramm combined with a binary result
+df.groupby('Survived').Fare.hist(alpha=0.6);
+
+#Plot a feature as a function of the binary result
+sns.swarmplot(x='Survived', y='Fare', data=df);
+
+#Pair plot
+sns.pairplot(df, hue='Survived');
