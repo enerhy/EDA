820669036fc56923d193aaf2e621b553ac96eb8b
Spasov <51121651+enerhy@users.noreply.github.com>
2019-11-06 14:02:44 +0200

    Create Arrays.py

---
+++ Arrays.py
@@ -0,0 +1,12 @@
+----Opearations with Arrays----
+
+#DataFrame to Array
+train = df.iloc[:].values
+
+#Getting Info
+np.info(train)
+
+#print the head
+print(train[:5,:])
+
+------Replacing NaN with 
